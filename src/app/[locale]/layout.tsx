08842ceab91f0e13555db4f6588850fc8148da12
Yuya Kanai <14835424+Cactice@users.noreply.github.com>
2024-09-29 00:50:11 +0900

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -36,10 +36,6 @@ const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   variable: '--font-space-grotesk'
 })
-export const metadata: Metadata = {
-  title: 'Next Temp',
-  description: 'create next app By Yahya Parvar!'
-}
 
 export default function RootLayout({
   children,
